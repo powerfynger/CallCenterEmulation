8892996e077c8c6983daa845c8607f7a832817e0
powerfyng <puunika@mail.ru>
2023-11-10 20:35:37 +0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "lib/json.hpp"
+#include <fstream>
+
+using json = nlohmann::json;
+
+
+int main(int, char**){
+    std::cout << "START\n";
+    std::ifstream f("..\\..\\config\\test.json");
+    // json data = json::parse(f);   
+    auto j = nlohmann::json::parse(f);
+    // Вывод содержимого объекта
+    for (auto& element : j["name"]) {
+        std::cout << element << "\n";
+    } 
+}
+
