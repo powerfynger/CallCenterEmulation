73ca485d115499a17c028868aefdbc5b0cbd9cbb
powerfynger <puunika@mail.ru>
2023-11-27 13:31:36 +0300

    (fix) Fixed args for enabling logs

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,8 @@ INITIALIZE_EASYLOGGINGPP
 int main(int argc, char** argv)
 {
   bool enableLogs;
-  if (argc >= 3 && strcmp(argv[1], "-l") == 0) {
-    enableLogs = atoi(argv[2]);
+  if (argc >= 2 && strcmp(argv[1], "-l") == 0) {
+    enableLogs = true;
   }
   el::Configurations defaultConf;
   defaultConf.setToDefault();
